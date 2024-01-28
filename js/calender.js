7c46e888e3db3bfaa0871831d0583a825a7e8290
Felix <f.j.ant@students.uu.nl>
2024-01-28 18:29:58 +0100

    Add navbar

--- js/calender.js
+++ js/calender.js
@@ -1,7 +1,7 @@
 // set the dimensions and margins of the graph
   const margin1 = {top: 20, right: 110, bottom: 0, left: 50},
      width1 = 830  - margin1.left - margin1.right,
-     height1 = 250 - margin1.top - margin1.bottom;
+     height1 = 300 - margin1.top - margin1.bottom;
 
 // append the svg object to the body of the page
 const svgjesse = d3.select("#calender-container")
